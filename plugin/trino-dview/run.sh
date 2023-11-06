60bcfcc9ce8466ffb927ad1d7ff0ba15a7480949
Shreyas@Dview <130682658+shreyas-dview@users.noreply.github.com>
2023-11-06 16:19:22 +0530

    Update run.sh

--- plugin/trino-dview/run.sh
+++ plugin/trino-dview/run.sh
@@ -11,6 +11,6 @@ docker run --name trino -d -p 8080:8080\
       --volume $PWD/target/trino-dview-430-SNAPSHOT:/usr/lib/trino/plugin/dview\
       --volume $PWD/src/main/resources/dview.properties:/etc/trino/catalog/dview.properties\
       --volume /Users/shreyasb/worskpace/dview/spring/schema-fortress/bin/test_data:/Users/shreyasb/worskpace/dview/spring/schema-fortress/bin/test_data\
-      -e accessKey=AKIAWL4FAPTQA5V2TGWK\
-      -e secretKey=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\
+      -e accessKey=${accessKey}\
+      -e secretKey=${secretKey}\
       trino-429-dview
